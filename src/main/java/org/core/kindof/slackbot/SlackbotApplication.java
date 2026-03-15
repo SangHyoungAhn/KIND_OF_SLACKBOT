31b06fe73a8192cafbc1cb5cea0ec50074d45bea
shahn0718 <shahn0718@gmail.com>
2026-03-15 19:11:39 +0900

    initial commit

---
+++ src/main/java/org/core/kindof/slackbot/SlackbotApplication.java
@@ -0,0 +1,13 @@
+package org.core.kindof.slackbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlackbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/core/kindof/slackbot/SlackbotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.core.kindof.slackbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
